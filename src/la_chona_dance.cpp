dc77e4d77d0986d5398c5a3187151a24beccb6d1
Alan G. Sanchez <sanchala@oregonstate.edu>
2024-11-22 16:33:23 -0800

    included addtional comments

--- src/la_chona_dance.cpp
+++ src/la_chona_dance.cpp
@@ -2,6 +2,7 @@
 #include "PointHeadClient.h"
 #include "Footwork.h"
 #include "FollowTrajectoryClient.h"
+#include "GripperClient.h"
 
 /**
  * @brief Entry point of the `fetch_move_node` program.
@@ -19,33 +20,43 @@ int main(int argc, char** argv) {
     // Initialize the ROS node
     ros::init(argc, argv, "fetch_move_node");
 
-     // // Create instances of `PointHeadClient`. This class provides functionality to interact 
-     // // with the robot's head controllers through ROS action servers.
+    // // Create instances of `PointHeadClient`. This class provides functionality to interact 
+    // // with the robot's head controllers through ROS action servers.
     PointHeadClient head_client;
 
-    // // 
-    Footwork base;
+    // // Create an instance of `GripperClient`. This class allows control over the robot's gripper,
+    // // enabling commands to open and close the gripper for object manipulation.
+    GripperClient gripper_client;
 
+    // // Create an instance of `Footwork`. This class provides functionality for base movements, 
+    // // such as linear motion and turning. It sends velocity commands to the robot's base.
+    Footwork base_cmd;
+
+    // // Create an instance of `FollowTrajectoryClient`. This class interacts with the 
+    // // robot's arm, allowing it to move to specified joint positions through a trajectory action.
     FollowTrajectoryClient trajectory_client;
 
     // Example: Move the robot arm to a predefined position
-    std::vector<double> positions = {0.4, -0.5, 0.2, 0.0, 0.5, 0.0, -0.4, 0.0};
-    if (trajectory_client.move_joints_to(positions, 2.0, "Forward", "Move")) {
+    // std::vector<double> positions = {0.4, -0.5, 0.2, 0.0, 0.5, 0.0, -0.4, 0.0};
+    if (trajectory_client.move_joints_to({0.4, -0.5, 0.2, 0.0, 0.5, 0.0, -0.4, 0.0}, 2.0, "Forward", "Move")) {
         ROS_INFO("Trajectory executed successfully!");
     } else {
         ROS_ERROR("Trajectory execution failed!");
     }
 
-    base.linear_motion(true, 10); // forward = true, num_publishes=10
-    base.turn(true, 10, true); // 
+    base_cmd.linear_motion(true, 10); // forward = true, num_publishes=10
+    base_cmd.turn(true, 10, true); // 
 
-    // // 
+    // // Command the robot's gripper to open and close. 
+    gripper_client.gripper_action(true);
+    gripper_client.gripper_action(false);
 
     // Point the robot's head to a target location (1.0, 0.0, 1.5) in the "base_link" frame
     // over a duration of 2 seconds.
-    head_client.lookAt(1.0, 0.0, 1.5, "base_link", 2.0);
+    head_client.lookAt(1.0, 0.0, 0.5, "base_link", 2.0);
+    head_client.lookAt(1.0, 0.0, 0.5, "gripper_link", 2.0);
 
-    // //
+    // // Keep the node running
     ros::spin();
 
     // Return 0 to indicate successful execution of the program.
