f14f36533c63e85d18c78be01512c280cee9bf02
Alan G. Sanchez <sanchala@oregonstate.edu>
2025-03-10 19:01:22 -0700

    included comments for imported message types.

--- src/Tools/relax_arm_control.cpp
+++ src/Tools/relax_arm_control.cpp
@@ -3,4 +3,23 @@
 // // and managing node execution
 #include <ros/ros.h>
 
-// // Import message type
+// // Import message types
+// // Provides the SimpleActionClient class for sending goals and receiving 
+// // results/feedback from ROS action servers.
+#include <actionlib/client/simple_action_client.h> 
+
+// // Includes message definitions for the QueryControllerStates action, such as goal, 
+// // result, and feedback structures.
+#include <robot_controllers_msgs/QueryControllerStatesAction.h> 
+
+// // STL container for managing dynamic arrays.
+#include <vector> 
+
+// // Sstandard library for string manipulation and storage
+#include <string> // Standard library for string manipulation and storage.
+
+int main(int argc, char** argv)
+{
+  
+  return 0;
+}
